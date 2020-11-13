7cf3971f00fd1e1643914aa9a3afff7b267ec5a4
Phyllip Hall <phall776@gmail.com>
2020-11-13 16:07:39 -0500

    Initial Commit

---
+++ list_issue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  list_issue
+//
+//  Created by Phyllip Hall on 11/13/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
